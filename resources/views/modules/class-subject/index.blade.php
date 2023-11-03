3e92082482403ef7a017d9b83d56a9afa0699c49
johnkenneth24 <kengerero@yahoo.com>
2023-11-03 15:26:21 +0800

    class sub

--- resources/views/modules/class-subject/index.blade.php
+++ resources/views/modules/class-subject/index.blade.php
@@ -37,7 +37,7 @@
                 <div class="modal-dialog" role="document">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">CREATE CLASS ADVISORY</h5>
+                      <h5 class="modal-title" id="exampleModalLabel">CREATE CLASS SUBJECT</h5>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                         </button>
                     </div>
