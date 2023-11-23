4826902f1fb8662de2ef597779a1896b66f6b73c
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2023-11-24 00:53:23 +0800

    modify seeder and ignore files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -47,45 +47,5 @@ class DatabaseSeeder extends Seeder
       ]);
       $user->assignRole('admin');
     }
-
-    if (!User::where('email', 'teacher@agvtfi.com')->first()) {
-      $user = User::create([
-        'school_id' => '00001',
-        'firstname' => 'Anna',
-        'middlename' => 'B',
-        'lastname' => 'Manalastas',
-        'gender' => 'Female',
-        'age' => '18',
-        'birthdate' => '1999-01-01',
-        'contact_number' => '09123456789',
-        'address' => 'Zone 5, Bulan, Sorsogoon',
-        'email' => 'teacher@agvtfi.com',
-        'password' => Hash::make('agvtfi54321'),
-      ]);
-      $user->assignRole('teacher');
-    }
-
-    if (!User::where('email', 'student@agvtfi.com')->first()) {
-      $user = User::create([
-        'school_id' => '00002',
-        'firstname' => 'Juan',
-        'middlename' => 'D',
-        'age' => '18',
-        'lastname' => 'Cruz',
-        'gender' => 'Male',
-        'birthdate' => '1999-01-01',
-        'contact_number' => '09123456789',
-        'address' => 'Zone 5, Bulan, Sorsogoon',
-        'email' => 'student@agvtfi.com',
-        'password' => Hash::make('agvtfi54321'),
-      ]);
-
-      StudentYearLevel::create([
-        'student_id' => $user->id,
-        'year_level' => 'Grade 11',
-      ]);
-
-      $user->assignRole('student');
-    }
   }
 }
